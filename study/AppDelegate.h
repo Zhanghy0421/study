abbc6a7c48e221e8d658b65be245242199737cda
张红岩 <zhanghongyan@zhanghongyandeiMac.local>
2019-11-10 16:25:36 +0800

    frist commit

---
+++ study/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  study
+//
+//  Created by 张红岩 on 2019/11/10.
+//  Copyright © 2019 张红岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ study/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  study
+//
+//  Created by 张红岩 on 2019/11/10.
+//  Copyright © 2019 张红岩. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor=[UIColor redColor];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
